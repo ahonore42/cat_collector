f8c1f4a847ea5d388800b2af0bd4303f31c22c4f
ahonore42 <a1honore80@gmail.com>
2020-09-21 14:01:55 -0400

    [views, urls] add update views and url patterns for cat instances

--- main_app/views.py
+++ main_app/views.py
@@ -1,16 +1,30 @@
 from django.shortcuts import render
 from .models import Cat
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.http import HttpResponseRedirect
 
 # Create your views (these are like your controller actions) here.
 # CREATE VIEW
-# djange will make 
+# djange will make a create cat form
 class CatCreate(CreateView):
     model = Cat
     fields = '__all__'
-    success_url = '/cats'
+    success_url = '/cats/'
 
-
+# UPDATE VIEW
+class CatUpdate(UpdateView):
+    model = Cat
+    fields = ['name', 'breed', 'description', 'age']
+    # django hook to grab the primary key (id) of a specific instance of the Cat model
+    # form valid will run automatically when this view is hit
+    def form_valid(self, form):
+        # don't immediately post to the db until we say so
+        self.object = form.save(commit=False)
+        # access the instance's primary key (id) with self.object.save()
+        self.object.save()
+        # redirect to the specific model instance's display page by adding the pk (id) to the url
+        return HttpResponseRedirect('/cats/' + str(self.object.pk))
+        
 def index(request):
     return render(request, 'index.html')
 
